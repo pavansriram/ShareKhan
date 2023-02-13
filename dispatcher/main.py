45f79290df5ed33da189924d43d1b2515e0ca83f
meera-04 <gsyamasameera2004@gmail.com>
2023-02-13 23:39:32 +0530

    Started Client Side Code

--- dispatcher/main.py
+++ dispatcher/main.py
@@ -1,11 +1,31 @@
 import socket
+from ..node.main import ResResource
+# import threads
 
 class Dispatcher:
     def __init__(self):
         self.activeThreads = 0
+        self.ServerSideSocket = socket.socket()
+        self.host = '127.0.0.1'
+        self.port = 5001
+        self.ThreadCount = 0
+        try:
+            self.ServerSideSocket.bind((self.host, self.port))
+        except socket.error as e:
+            print(str(e))
+        print('Socket is listening..')
+        self.ServerSideSocket.listen(5)
         
 
     def ListenForever(self):
+        while True:
+            Client, address = self.ServerSideSocket.accept()
+            print('Connected to: ' + address[0] + ':' + str(address[1]))
+            start_new_thread(ResResource, (Client, ))
+            ThreadCount += 1
+            print('Thread Number: ' + str(ThreadCount))
+        ServerSideSocket.close()
+
         serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serverSocket.bind((socket.gethostname(), 80))
         serverSocket.listen(5)
--- node/main.py
+++ node/main.py
@@ -1,4 +1,7 @@
 import socket
+from hash import keyOfResource
+import pickle
+import os
 
 class Node:
     def __init_(self, ipAddr):
@@ -8,6 +11,9 @@ class Node:
         self.key = self.ComputeKey(ipAddr)
         self.fingerTable = self.ComputeFingerTable()
         self.myResources = []
+        self.m = 3
+        self.totalNodes = 8
+        self.maxFileSize = 4096
 
     def ComputeKey(ipAddr):
         return 0
@@ -15,20 +21,35 @@ class Node:
 
     # Request for a resource
     def ReqResource(self, filename):
+        destIpAddr, destPort = self.Lookup(filename)
+
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.connect((destIpAddr, destPort))
+
+        clientSocket.send(pickle.dumps('ResourceRequest', filename))
+        data = pickle.loads(clientSocket.recv(self.maxFileSize))
         pass
 
     # Send the requested resource as response
-    def ResResource():
+    def ResResource(connection):
+        connection.send(str.encode('Server is working:'))
+        while True:
+            data = connection.recv(2048)
+            response = 'Server message: ' + data.decode('utf-8')
+            if not data:
+                break
+            connection.sendall(str.encode(response))
+        connection.close()
         pass
 
 
     def Lookup(self, filename):
-        filekey = self.keyOfResource(filename)
+        filekey = keyOfResource(filename)
         if self.predecessor < filekey and filekey <= self.key:
-            return self.key
+            return (self.ipAddr, self.port)
         
         for l in reversed(self.fingerTable):
-            if filekey <= l.key:
+            if (filekey - self.key + self.totalNodes)%self.totalNodes >= (l.key - self.key + self.totalNodes)%self.totalNodes:
                 return RPC('Lookup', l.key, filekey)
 
         return RPC('Lookup', self.fingerTable[0].key, filekey)
