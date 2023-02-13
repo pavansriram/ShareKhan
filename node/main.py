c9d9c5d4ad5bf1c2f16d2e3b6bfbd8ad43fcbff2
cs19b014iittp <70994761+cs19b014iittp@users.noreply.github.com>
2023-02-13 16:20:05 +0000

    changed lookup function

--- node/main.py
+++ node/main.py
@@ -3,11 +3,14 @@ import socket
 class Node:
     def __init_(self, ipAddr):
         self.ipAddr = ipAddr
+        self.predecessor = None
+        self.successor = None
         self.key = self.ComputeKey(ipAddr)
         self.fingerTable = self.ComputeFingerTable()
         self.myResources = []
 
     def ComputeKey(ipAddr):
+        return 0
         pass
 
     # Request for a resource
@@ -18,7 +21,19 @@ class Node:
     def ResResource():
         pass
 
+
     def Lookup(self, filename):
+        filekey = self.keyOfResource(filename)
+        if self.predecessor < filekey and filekey <= self.key:
+            return self.key
+        
+        for l in reversed(self.fingerTable):
+            if filekey <= l.key:
+                return RPC('Lookup', l.key, filekey)
+
+        return RPC('Lookup', self.fingerTable[0].key, filekey)
+
+        '''
         (successorIp, key) = self.FindSuccessor(filename)
         
         # send request till we find the resource containing machine
@@ -27,6 +42,8 @@ class Node:
             clientSocket.connect((successorIp, 80))
             # .....
             pass
+        '''
+        
     def ComputeFingerTable():
         pass
 
